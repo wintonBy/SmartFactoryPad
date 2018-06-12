387c958740fb205ac3b94c68d2547da142beb8ff
Winton <winton_by@126.com>
2018-06-12 21:39:11 +0800

    功能开发

---
+++ app/src/main/java/com/af/smartfactorypad/network/response/LoginResponse.kt
@@ -0,0 +1,13 @@
+package com.af.smartfactorypad.network.response
+
+/**
+ * @author: winton
+ * @time: 2018/6/12 21:11
+ * @package: com.af.smartfactorypad.network.response
+ * @project: SmartFactoryPad
+ * @mail:
+ * @describe: 登录的响应体
+ */
+class LoginResponse : BaseResponse(){
+
+}
--- app/src/main/java/com/af/smartfactorypad/ui/fragment/CallFragment.java
+++ app/src/main/java/com/af/smartfactorypad/ui/fragment/CallFragment.java
@@ -1,14 +1,19 @@
 package com.af.smartfactorypad.ui.fragment;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.af.smartfactorypad.R;
+import com.zxing.activity.CodeUtils;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * @author: winton
@@ -19,6 +24,9 @@ import butterknife.ButterKnife;
  * @describe: 呼叫页
  */
 public class CallFragment extends BaseFragment {
+
+    @BindView(R.id.iv_my_code)
+    ImageView mIVCode;
 
     public static CallFragment newInstance(Bundle params){
         CallFragment fragment = new CallFragment();
@@ -33,6 +41,35 @@ public class CallFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.frag_call_service,null);
         ButterKnife.bind(this,view);
+        initData();
         return view;
     }
+
+    private void initData(){
+       mIVCode.post(new Runnable() {
+           @Override
+           public void run() {
+               Bitmap codeImage = CodeUtils.createImage("12134",mIVCode.getHeight(),mIVCode.getHeight(),null);
+               mIVCode.setImageBitmap(codeImage);
+           }
+       });
+    }
+
+    @OnClick({R.id.call_manager,R.id.call_tech,R.id.call_logistics,R.id.call_quality,R.id.call_support})
+    public void clickCall(View view){
+        switch (view.getId()){
+            case R.id.call_manager:
+                break;
+            case R.id.call_logistics:
+                break;
+            case R.id.call_tech:
+                break;
+            case R.id.call_support:
+                break;
+            case R.id.call_quality:
+                break;
+        }
+    }
+
+
 }
